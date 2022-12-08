e7314d53e6cdfcf418446faae4131275de55493b
Moises <neto_guedes_1@hotmail.com>
2022-12-08 11:10:58 +0100

    Let's Travel - Full Project git status git log
    git status

---
+++ public/admin/js/update-post.js
@@ -0,0 +1,46 @@
+{
+    let articlesBlock = document.querySelector('.articles-list');
+    let updateBtn = document.querySelector('#v-pills-update-post-tab');
+    let updateForm = document.querySelector('.update-post-form');
+    let id;
+    let titleInput = document.querySelector('#update-title');
+    let textArea = document.querySelector('#update-text');
+    let cancelBtn = document.querySelector('#cancel-btn');
+
+    articlesBlock.addEventListener('click', async function(e) {
+        if(e.target.classList.contains('edit-btn')) {
+            id = e.target.parentNode.parentNode.querySelector('.id').value;
+            let postInfo = await fetch('http://localhost:3000/posts/' + id)
+                .then((response) => response.json())
+                .then((data) => data)
+            titleInput.value = postInfo.title;
+            textArea.value = postInfo.text;
+            updateBtn.click();
+        }
+    })
+
+    updateForm.addEventListener('submit', async function(e) {
+        e.preventDefault();
+        let updateDrescription;
+        if(textArea.value.indexOf('.') === -1){
+            updateDrescription = textArea.value;
+        }else {
+            updateDrescription = textArea.value.substring(0, textArea.value.indexOf('.') + 1);
+        }
+        fetch('http://localhost:3000/posts/' + id, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: titleInput.value,
+                text: textArea.value,
+                description: updateDrescription
+            })
+        }).then((res) => res.text()).then(() => window.history.go());
+    })
+
+    cancelBtn.addEventListener('click', function() {
+         window.history.go()    
+      })
+}
---
+++ public/js/maps.js
@@ -0,0 +1,25 @@
+var platform = new H.service.Platform({
+    'apikey': 'mXuSQhjf0JrkbwFfH0wHruOzp9S5OLRLDFI9Bhsglw0'
+  });
+  
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+// Get an instance of the geocoding service:
+var service = platform.getSearchService();
+
+let lendmark = document.querySelector('.main-heading').textContent;
+service.geocode({
+    q: lendmark
+  }, (result) => {
+    var map = new H.Map(
+        document.querySelector('.map'),
+        defaultLayers.vector.normal.map,
+        {
+          zoom: 15,
+          center: result.items[0].position
+        });
+    map.addObject(new H.map.Marker(result.items[0].position));    
+    var ui = H.ui.UI.createDefault(map, defaultLayers);
+  }, alert);
+
+  
